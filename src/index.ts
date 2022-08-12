392d95644e38c15eec884a515f5966e9132b9f6a
typhonrt <support@typhonjs.io>
2022-08-11 19:27:04 -0700

    added data types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
-export * from './array/index.js';
-export * from './map/index.js';
+export *                    from './array/index.js';
+export *                    from './map/index.js';
+
+export type {
+    IDerivedReducerCtor,
+    IDerivedReducer }       from './types/interface.js';
+
+export type {
+    DataDynArray,
+    DataDynMap,
+    DataFilter,
+    DataSort,
+    CompareFn,
+    FilterFn,
+    DataDerived,
+    DataDerivedOptions,
+    OptionsDerivedCreate }  from './types/data.js';
