8e0ee076d9e5e76b412ee9a19190d491a5795023
typhonrt <support@typhonjs.io>
2024-03-26 14:25:08 -0700

    Update config.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,6 @@
-import alias               from '@rollup/plugin-alias';
 import typescript          from '@rollup/plugin-typescript';
-import dts                 from 'rollup-plugin-dts';
+import { generateDTS }     from '@typhonjs-build-test/esm-d-ts';
+import { importsResolve }  from "@typhonjs-build-test/rollup-plugin-pkg-imports";
 
 // Produce sourcemaps or not.
 const s_SOURCEMAP = true;
@@ -8,7 +8,7 @@ const s_SOURCEMAP = true;
 export default () =>
 {
    return [{   // This bundle is for the Node distribution.
-         input: ['src/index.ts'],
+         input: 'src/index.ts',
          output: [{
             file: `./dist/index.js`,
             format: 'es',
@@ -16,30 +16,9 @@ export default () =>
             sourcemap: s_SOURCEMAP,
          }],
          plugins: [
-            alias({
-               entries: [
-                  { find: '#common', replacement: './src/common/index.js' }
-               ]
-            }),
-            typescript()
-         ]
-      },
-
-      {   // This bundle is for the Node distribution.
-         input: ['src/index.ts'],
-         output: [{
-            file: `./types/index.d.ts`,
-            format: 'es',
-            sourcemap: false
-         }],
-         plugins: [
-            alias({
-               entries: [
-                  { find: '#common', replacement: './src/common/index.js' }
-               ]
-            }),
-            typescript({ sourceMap: false, inlineSources: false }),
-            dts()
+            importsResolve(),
+            typescript(),
+            generateDTS.plugin()
          ]
       }
    ];
