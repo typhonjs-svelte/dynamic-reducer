78972c2b82285ce5ecd9c08e4779cefbed37b8e2
typhonrt <support@typhonjs.io>
2024-12-27 01:32:29 -0800

    Remove old tests.

--- test/src/runner/tests/array/index.js
+++
@@ -1,3 +0,0 @@
-export * as ArrayAPIErrors       from './APIErrors.js';
-export * as ArrayAPITests        from './APITests.js';
-export * as ArrayDerivedTests    from './DerivedTests.js';
--- test/src/runner/tests/map/index.js
+++
@@ -1,3 +0,0 @@
-export * as MapAPIErrors      from './APIErrors.js';
-export * as MapAPITests       from './APITests.js';
-export * as MapDerivedTests   from './DerivedTests.js';
